b1aee0c3ee10568423771f1006fecce6a9c920e5
Adam Socki <socki.adam@gmail.com>
2024-04-28 13:43:41 -0500

    gui work

--- DebugManager.gd
+++ DebugManager.gd
@@ -1,18 +1,28 @@
 extends Node
 
 
+
 @export_group("Debug Properties")
 @export var debugMode: bool
 
 @export_group("Debug Properties")
 @export var vSync: bool
 
+@export_group("Debug Properties")
+@export var showGui: bool
 
 func InitDebugManager():
 	if (vSync):
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
 	else:
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+		
+		
+	if (showGui):
+		$"../UIManager".get_node("InGameUI").visible = true
+	else:
+		$"../UIManager".get_node("InGameUI").visible = false
+		
 
 
 func UpdateDebugManager(delta):
